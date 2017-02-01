02c34b30f5c6b6c45c46857c5dd8e3e2d86a9cdf
Valeriy Shtayts <bajiepkasmile@yandex.ru>
2017-02-01 11:21:46 +0300

    Added BaseFragment

---
+++ app/src/main/java/com/nodomain/mensclothingstore/ui/fragments/BaseFragment.java
@@ -0,0 +1,33 @@
+package com.nodomain.mensclothingstore.ui.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+
+import com.nodomain.mensclothingstore.mvp.presenters.MvpPresenter;
+import com.nodomain.mensclothingstore.mvp.views.MvpView;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public abstract class BaseFragment<P extends MvpPresenter> extends Fragment implements MvpView {
+
+    protected P mvpPresenter;
+
+    private Unbinder unbinder;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+        mvpPresenter.attachMvpView(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        mvpPresenter.detachMvpView();
+        unbinder.unbind();
+        super.onDestroyView();
+    }
+}
